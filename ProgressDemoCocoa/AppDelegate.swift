dc8019b29405bb7affe30f9f666c1bbf903b7013
code8086 <code8086@qq.com>
2015-07-26 15:42:30 +0800

    first commit

---
+++ ProgressDemoCocoa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProgressDemoCocoa
+//
+//  Created by code8086 on 15/7/26.
+//  Copyright (c) 2015年 code8086. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProgressDemoCocoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProgressDemoCocoa
+//
+//  Created by code8086 on 15/7/26.
+//  Copyright (c) 2015年 code8086. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        super.title = "test";
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
